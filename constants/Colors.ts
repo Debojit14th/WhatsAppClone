6ccddde403a1ee948881bd15277574865ea47c10
Debojit14th <14thdebojit@gmail.com>
2025-07-22 15:45:45 +0530

    Add files via upload

---
+++ constants/Colors.ts
@@ -0,0 +1,11 @@
+export default {
+  primary: '#1063FD',
+  muted: '#3A5A92',
+  background: '#EFEEF6',
+  grey: '#6E6E73',
+  lightgrey: '#DCDCE2',
+  green: '#4FEE57',
+  lightgreen: '#D8FFCB',
+  red: '#EF0827',
+  yellow: '#FCC70B'
+};
